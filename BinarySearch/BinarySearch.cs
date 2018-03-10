1e17660603f56e09e3abb22cc52088b811d26e3c
Jason Thatcher <contact@jasonthatcher.com>
2018-03-10 17:39:56 -0600

    Update Binary Search Function

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class BinarySearch
+    {
+        int[] workingList;
+        int left;
+        int right;
+        int match;
+
+        //List must be sorted. 
+        public BinarySearch(int[] sortedList)
+        {
+            workingList = sortedList;
+            left = 0;
+            right = sortedList.Length-1;
+        }
+
+        public int Match { get => match; set => match = value; }
+                
+        public int Search(int lookup)
+        {
+            Console.Write("Starting List: ");
+            Print(left,right);
+            Console.Write("Secret Number is {0} \n\n", lookup.ToString());
+
+            while (left <= right)
+            {
+                int guess = (right + left) / 2;
+                Console.Write("Guess: {0} ", workingList[guess].ToString());                
+                
+                if(workingList[guess] == lookup)
+                {
+                    Console.Write(" MATCH!\n");
+                    return guess;
+                }
+                else if(workingList[guess] > lookup)
+                {   
+                    //Since guess was too big set right side of list to guess - 1 and guess again.
+                    right = guess - 1;
+                    #region Print
+                            Console.Write(" TOO BIG\n");
+                            Console.Write("Reduce list.  New List: ");
+                            Print(left, right);
+                            Console.WriteLine();
+                            #endregion
+                }                
+                else
+                {
+                    //Since guess was too small set left of list to guess + 1 and guess again.
+                    left = guess + 1;
+                    #region Print
+                    Console.Write(" TOO small\n");
+                    Console.Write("Reduce list.  New List: ");
+                    Print(left, right);
+                    Console.WriteLine();
+                    #endregion
+                }
+            }
+            return -1;
+        }
+
+        private void Print()
+        {
+            for(int i = left; i<right; i++)
+            {
+                Console.Write(workingList[i].ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private void Print(int left, int right)
+        {
+            for (int i = left; i < right+1; i++)
+            {
+                Console.Write(workingList[i].ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -10,6 +10,19 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
+            int[] myList = new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+            Console.Write("Enter the secret number between 1 - 20: ");
+            int.TryParse(Console.ReadLine(), out int v);
+            Console.WriteLine();
+
+            BinarySearch bs = new BinarySearch(myList);
+            if (bs.Search(v) == -1)
+            {
+                Console.WriteLine("Could Not Match!");
+            }
+            
+            Console.ReadKey();
         }
     }
 }
